458c03ad27db3eb9ab2ec794221d63040949f036
Alyssa Yates <bosslys@icloud.com>
2019-10-30 23:01:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,8 +19,8 @@ class App < Sinatra::Base
     params[:word1] + (' ') + params[:word2] + (' ') + params[:word3] + (' ') + params[:word4] + (' ') + params[:word5] + ('.')
   end
 
-  get '/:operation/:number1/:number2' do
-    @answer = params[:number1].to_i + params[:number2].to_i
+  get '/:operation/:number1/:number2/:operation' do
+    @answer = params[:number1].to_i params[:operation] params[:number2].to_i
   	@answer.to_s
   end
 
